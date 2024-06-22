17a8c4ac003630f2b6d8ab7cd2ec18675d184afb
Arpit Raj <113095887+arpitraj96@users.noreply.github.com>
2024-06-22 19:04:32 +0000

    learn concept of Data Types

---
+++ JavaScript/Basics/02_DataTypes.js
@@ -0,0 +1,25 @@
+"use strict";  // It treat all JS code as newer version
+//alert(3+3)  // 
+
+let name = "Arpit"
+let age = 18
+let isLoggedIn = false
+
+/*
+Primitive Data Types
+number => 2^53
+bigint
+string => ""
+boolean => true/false
+null => standalone value
+undefined => 
+symbol => unique
+
+Object Data Types
+
+*/
+
+console.log(typeof "Arpit");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
