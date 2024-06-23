7f9665d992539548c2fc64492c4dc32f8ad13c36
Arpit Raj <113095887+arpitraj96@users.noreply.github.com>
2024-06-23 07:41:31 +0000

    more about DataTypes

--- JavaScript/Basics/02_DataTypes.js
+++ JavaScript/Basics/02_DataTypes.js
@@ -23,3 +23,31 @@ console.log(typeof "Arpit");
 console.log(typeof age);
 console.log(typeof null);
 console.log(typeof undefined);
+
+
+/************** More about Data Types **************/
+/* 
+// Primitive Data Types (Call by Value (Copy))
+7 types => String, Number, Boolean, Null, Undefined, Symbol, BigInt, 
+
+// Reference (Non - Primitive) -> Data Types that are derived from Primitive Data Types
+Types => Array, Object, Functinons
+*/
+const arr = ["Apple", "Boy", "Cat", "Dog", "Eye"]  //Array in JS -> typeof -> object
+let myObj = {    //Object in JS -> typeof -> object
+    name: "Alex",
+    age: 22,
+    gender: "Male"
+}
+console.log(arr);
+console.log(myObj);
+
+
+const myFunction = function () {  //Functions in JS -> typeof -> functions
+    console.log("Hello");
+}
+myFunction()
+
+console.log(typeof arr);
+console.log(typeof myObj);
+console.log(typeof myFunction);
